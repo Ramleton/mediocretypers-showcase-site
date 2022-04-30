b7f82f498cde40142e8c8ec3ead9502e214b836a
Ramleton <ishaansaini@outlook.com>
2022-04-30 13:54:06 -0400

    Added NavBar.jsx and modified index.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,14 @@
 // import React from 'react';
 
+import Link from 'next/link'
+
 // eslint-disable-next-line no-unused-vars
-const NavBar = () => {};
+const NavBar = () => {
+    return <nav>
+        <Link href="/">Home</Link>
+        <Link href="/ishaan">Ishaan's Page</Link>
+        <Link href="/lanz">Lanz's Page</Link>
+    </nav>;
+};
 
 export default NavBar;
