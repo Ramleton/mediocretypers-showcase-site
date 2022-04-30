9433fc6b36f2c737a1fe4c9079afe205ab958c4a
Ramleton <ishaansaini@outlook.com>
2022-04-30 15:35:58 -0400

    Added some content to my page

--- pages/ishaan.jsx
+++ pages/ishaan.jsx
@@ -1,5 +1,30 @@
 const Ishaan = () => {
-    return <div>Ishaan's Page</div>;
+    return <div>
+            <h1>Welcome to my page!</h1>
+            <p></p>
+            <h2>Experienced in</h2>
+            <l>
+                <li>Python</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Java</li>
+                <li>TypeScript</li>
+                <li>React</li>
+                <li>Data Structures</li>
+            </l>
+            <h2>Projects</h2>
+            <h3>Guardian Tales Team Builder</h3>
+            <h3>Three Musketeers (with some extra features)</h3>
+            <h3>And this personal website</h3>
+            <footer>Contact</footer>
+            <l>
+                <li>Github
+                    <img src="https://avatars.githubusercontent.com/u/77259553?v=4"></img>
+                </li>
+                <li>Email: ishaan.saini@student.utoronto.ca</li>
+            </l>
+        </div>;
 };
 
 export default Ishaan;
