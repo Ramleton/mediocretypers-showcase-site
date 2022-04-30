79261170097641d4ef37f6b7c8cc02d51b685a75
Ramleton <ishaansaini@outlook.com>
2022-04-30 19:39:34 -0400

    Added my image to the home page

--- pages/aboutIshaan.jsx
+++ pages/aboutIshaan.jsx
@@ -8,6 +8,8 @@ const AboutIshaan = () => {
                 Hi, I'm a 2nd year computer science student at UTM.<br />
                 If you want to learn more about me, click on my name at the navbar!
             </p>
+            <p>Here's a picture of me!</p>
+            <img className={styles.portrait} src="https://i.imgur.com/cAQIqnm.jpg"></img>
         </div>
     </div>;
 }
