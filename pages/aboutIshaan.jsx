58b4d79db4984e35241390a2c035972cb36c7c4a
Ramleton <ishaansaini@outlook.com>
2022-04-30 15:15:59 -0400

    Added our respective about pages

---
+++ pages/aboutIshaan.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/AboutIshaan.module.css";
+
+const AboutIshaan = () => {
+    return <div className={styles.aboutishaan}>
+        <div className={styles.text}>
+            <h1>Ishaan</h1>
+            <p>
+                Hi, I'm a 2nd year computer science student at UTM.<br />
+                If you want to learn more about me, click on my name at the navbar!
+            </p>
+        </div>
+    </div>;
+}
+
+export default AboutIshaan;
---
+++ pages/aboutlanz.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/AboutLanz.module.css";
+
+const AboutLanz = () => {
+    return <div className={styles.aboutlanz}>
+        <div className={styles.text}>
+            <h1>Lanz</h1>
+            <p>
+                Hi, I'm a 2nd year computer science student at UTM.<br />
+                If you want to learn more about me, click on my name at the navbar!
+            </p>
+        </div>
+    </div>;
+}
+
+export default AboutLanz;
