7f1fa8359ab3e1692ff63a4ed84cc153c178905f
Bodyshots <masterofbodyshots@gmail.com>
2022-05-01 09:03:52 -0400

    edited my about desc a bit

--- pages/aboutlanz.jsx
+++ pages/aboutlanz.jsx
@@ -6,9 +6,9 @@ const AboutLanz = () => {
             <h1>Lanz</h1>
             <p>
                 Hi, I'm a 2nd year Computer Science and Sociology at UTM.<br/>
-                If you want to learn more about me, click on my name at the navbar!
+                Click on my name on the navigation bar above to know more!
             </p>
-            <p>Here's my handsome face!</p>
+            <p>Here's my handsome face, by the way!</p>
             <img className={styles.portrait} src="https://i.imgur.com/Qu63YAy.jpg"></img>
         </div>
     </div>;
