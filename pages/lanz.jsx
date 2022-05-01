91198782e55e533999da7b6d5a55d58033d0dc16
Bodyshots <masterofbodyshots@gmail.com>
2022-05-01 03:28:22 -0400

    one image done pausechamp

--- pages/lanz.jsx
+++ pages/lanz.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LanzIntro from './lanzintro';
 import LanzSkills from './lanzskills';
 import LanzGit from './lanzgit';
+import Link from "next/link";
 import LanzContact from './lanzcontact';
 import styles from "../styles/Lanz.module.css";
 
@@ -9,9 +10,12 @@ const lanz = () => (
     <div className={styles.lanz}>
       <LanzIntro></LanzIntro>
       <LanzGit></LanzGit>
+      <Link href="https://github.com/Ramleton/mediocretypers-showcase-site">
+        <h1 className={styles.clickable}>..and this website!</h1>
+      </Link>
       <div className={styles.skills_contacts_row}>
-      <LanzSkills></LanzSkills>
-      <LanzContact></LanzContact>
+        <LanzSkills></LanzSkills>
+        <LanzContact></LanzContact>
       </div>
     </div>
 );
--- pages/lanzgit.jsx
+++ pages/lanzgit.jsx
@@ -3,74 +3,78 @@ import Link from "next/link";
 import GithubButton from '../components/GithubButton';
 import Project from '../components/Project';
 import styles from "../styles/Lanzgit.module.css";
-import styles2 from "../styles/Lanzgitproj2.module.css"
 
 const lanzgit = () => (
     <div className={styles.lanzgit}>
-        <avatar_pos>
-          <GithubButton path="https://github.com/Bodyshots" avatar_path="https://avatars.githubusercontent.com/u/67764300?v=4" avatar_style={styles.avatar}>
-          </GithubButton>
-        </avatar_pos>
-        <Link href="https://github.com/Bodyshots">
-            <a className={styles.title}> Github </a>
-        </Link>
-        <p> 
-            Here's where you can find all of my projects. Currently, I have three (non-school related) projects.
-            Click any of them to go to their respective source code pages:
-        </p>
-        <projects>
-            <Link href="https://github.com/Bodyshots/seducer-bootleg">
-                <h2 className={styles.title}>(Not) Super Seducer: The Bootleg Version (Python)</h2>
-            </Link>
-            <Project project_name="(Not) Super Seducer: The Bootleg Version" project_img_src="https://i.imgur.com/D03IXlf.png"
-            project_desc={"Game in-action. Depending on your choices, you will either gain or lose \"confidence.\""} project_styles={styles}>
-            </Project>
-            <projecttext>
-                A single-player dating game in which the player has to endure a series of questions. 
-                Depending on your date's personality and how you respond to a question, you may gain or lose confidence
-                in yourself. Go down to 0% confidence and it's game over.
-                <br />
-                <br />
-                This features: <li>Multiple 8-bit/16-bit songs! </li>
-                               <li>4 different personalities to date!</li>
-                               <li>50+ dating questions!</li>
-                               <li>3 Difficulty levels to choose from!</li>
-                <br />
-                <br />
-            Play the game to try and find out if you can endure the ruthless questioning from your date!
-            </projecttext>
-            <br />
-            <br />
-            <Link href="https://github.com/Bodyshots/MassFolderCreator">
-                <h2 className={styles.title}>Mass File Creator (Java)</h2>
-            </Link>
-            <Project project_name="Mass Folder Creator" project_img_src="https://i.imgur.com/Mr8Acil.png"
-            project_desc={"An example use case. Here, we try to create 5 numbered folders named \"Week\"."} project_styles={styles2}>
-            </Project>
-            <projecttext>
-                A simple program that allows you to create multiple folders at once,
-                wherever you are. 
-                <br />
-                <br />
-                This includes: <li>Creating the same folder in multiple directories</li>
-                               <li>Creating at some valid path (including where the program is run)</li>
-                <br />
-                <br />
-                The program also has a "Create Week folders" option, which is great tool in preparing for the 
-                next semester. Just input your year, course code, and number of weeks. 
-                <br />
-                <br />
-                Thereafter, the program will create: <li>A year folder</li>
-                                                     <li>A course folder, inside the year folder</li>
-                                                     <li>X week folders inside your course folder</li>
-                <br />
-                <br />
-            </projecttext>
-            <br />
-            <br />
-            <b>This website!</b>
-        </projects>
+        <div className={styles.column}>
+          <div>
+              <div className={styles.pic_pos}>
+              <GithubButton path="https://github.com/Bodyshots" avatar_path="https://avatars.githubusercontent.com/u/67764300?v=4" avatar_style={
+                  styles.githubavatar}>
+              </GithubButton>
+              </div>
+            <div>
+              <div className={styles.proj_pic1}>
+                <Project project_name="(Not) Super Seducer: The Bootleg Version" project_img_src="https://i.imgur.com/D03IXlf.png"
+                         project_desc={"Game in-action. Depending on your choices, you will either gain or lose \"confidence.\""} project_styles={
+                         styles}>
+                </Project>
+              </div>
+              <div className={styles.proj_pic2}>
+                <Project project_name="Mass Folder Creator" project_img_src="https://i.imgur.com/Mr8Acil.png"
+                         project_desc={"An example use case. Here, we try to create 5 numbered folders named \"Week\"."} project_styles={
+                         styles}>
+                </Project>
+               </div>
+            </div>
+        </div>
+    </div>
+        <div className={styles.column}>
+          <Link href="https://github.com/Bodyshots">
+            <h2 className={styles.clickable}>Github </h2>
+          </Link>
+          <p className={styles.proj_text}>Here's where you can find all of my projects. Currently, I have three (non-school related) projects.
+           Click any of them to go to their respective source code pages:</p>
+          <Link href="https://github.com/Bodyshots/seducer-bootleg">
+            <h2 className={styles.clickable}>(Not) Super Seducer: The Bootleg Version (Python)</h2>
+          </Link>
+          <p className={styles.proj_text}>A single-player dating game in which the player has to endure a series of questions.
+          Depending on your date's personality and how you respond to a question, you may gain or lose confidence
+          in yourself. Go down to 0% confidence and it's game over.
+          <br />
+          <br />
+          This features:</p><br />
+                         <li>Multiple 8-bit/16-bit songs! </li>
+                         <li>4 different personalities to date!</li>
+                         <li>50+ dating questions!</li>
+                         <li>3 Difficulty levels to choose from!</li>
+          <br />
+          <p className={styles.proj_text}>Play the game to try and find out if you can endure the ruthless questioning from your
+          date!</p>
+          <br />
+          <Link href="https://github.com/Bodyshots/MassFolderCreator">
+            <h2 className={styles.clickable}>Mass File Creator (Java)</h2>
+          </Link>
+          <p className={styles.proj_text}>A simple program that allows you to create multiple folders at once,
+          wherever you are.
+          <br />
+          <br />
+          This includes:</p><br />
+                         <li>Creating the same folder in multiple directories</li>
+                         <li>Creating at some valid path (including where the program is run)</li>
+          <br />
+          <p className={styles.proj_text}>The program also has a "Create Week folders" option, which is great tool for preparing for the
+          next semester. Just input your year, course code, and number of weeks.
+          <br />
+          <br />
+          Thereafter, the program will create:</p><br />
+                                              <li>A year folder</li>
+                                              <li>A course folder, inside the year folder</li>
+                                              <li>X week folders inside your course folder</li>
+          <br />
+          <br />
 
+        </div>
     </div>
 );
 
