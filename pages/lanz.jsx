3201b9c2b6318c4a896dcb9834900142dfe77b33
Bodyshots <masterofbodyshots@gmail.com>
2022-05-01 00:50:37 -0400

    fixed white spacing on lanz page i guess???

--- pages/lanz.jsx
+++ pages/lanz.jsx
@@ -3,13 +3,16 @@ import LanzIntro from './lanzintro';
 import LanzSkills from './lanzskills';
 import LanzGit from './lanzgit';
 import LanzContact from './lanzcontact';
+import styles from "../styles/Lanz.module.css";
 
 const lanz = () => (
-    <div>
+    <div className={styles.lanz}>
       <LanzIntro></LanzIntro>
       <LanzGit></LanzGit>
+      <div className={styles.skills_contacts_row}>
       <LanzSkills></LanzSkills>
       <LanzContact></LanzContact>
+      </div>
     </div>
 );
 
